c9abe4ce249baa795b690f27e5602e2695984ed1
clarkj99 <clark@stontech.net>
2019-11-18 11:58:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 /* Your code goes here */
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+};
+
+function overWriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+};
+
+const leastFavoriteCustomer = 'mary';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'sam';
+};
+
+
+
